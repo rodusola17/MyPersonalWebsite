2f684299f03310c82128f81973cc6a85a226c781
rodusola <rodusola@cps.edu>
2016-04-25 14:07:04 +0000

    first commit

---
+++ app/controllers/exams_controller.rb
@@ -0,0 +1,24 @@
+class ExamsController<ApplicationController
+      
+      
+      def main
+          render "main"
+          
+      end
+      
+      def personal
+          render "personal"
+          
+      end
+      
+      def sports
+          render "sports"
+            
+      end
+      
+      def hobbies
+          render "hobbies"
+          
+      end      
+    
+end
